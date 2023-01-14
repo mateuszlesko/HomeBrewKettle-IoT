7ce63b3d9c8c0e0f8b77bf9521ee302a59916f69
mateuszlesko <mateusz.lesko@outlook.com>
2023-01-14 13:54:02 +0100

    refactory

--- core/Inc/main.h
+++ core/Inc/main.h
@@ -4,51 +4,12 @@
 
 #include <stdio.h>
 #include <stddef.h>
-<<<<<<< HEAD
-=======
 #include <string.h>
->>>>>>> a7b3513bb6b34af888b0dd8813c3e9e4c16e4598
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "freertos/semphr.h"
 #include "driver/timer.h"
-<<<<<<< HEAD
-
-#include "../../app/PINOUT/Pinout.h"
-#include "../../app/ADC/PinADC1.h"
-#include "../../app/SENSORS/LM35.h"
-
-#define SETUP_LOG_TAG "SETUP"
-#define MAIN_LOG_TAG "MAIN"
-#define LOG_LEVEL_LOCAL ESP_LOG_VERBOSE
-
-#define MAX_RECEPTURE_STAGES 10
-
-#define TIMER_DIVIDER   (16)
-#define pdSECOND pdMS_TO_TICKS(1000)
-#define CYCLES_TO_MEASUREMENT 12000
-
-
-extern "C"
-{
-    static bool IRAM_ATTR timer_group_isr_callback(void * args);
-}
-
-static SemaphoreHandle_t intervalTimerSem;
-
-static int cyclesCounter = 0;
-
-class Main final{
-    public:
-        Main(void);
-        esp_err_t setupHardware(void);
-        //esp_err_t setupTimer(void);
-        void ledsTest(void);
-        void run(void);
-};
-
-=======
 #include "driver/gpio.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
@@ -83,5 +44,4 @@ static SystemState actual_state = RESET;
 #define FINISH_PROCESS_BIT 7
 
 
->>>>>>> a7b3513bb6b34af888b0dd8813c3e9e4c16e4598
 #endif
